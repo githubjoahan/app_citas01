1007f28b6b98a541c4ed57049765a835b22f314b
githubjoahan <joahan.alvaro8@gmail.com>
2023-10-26 02:01:24 -0600

    tercer cambio

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload joahan gutierrers alvaro y adriana cecilia marcelin araujo. 
+            Edit <code>src/App.js</code> joahan. 
           </p>
           <a
             className="App-link"
